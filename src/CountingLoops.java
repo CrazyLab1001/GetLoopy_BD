4fc0b8ddf79883e47b469b34377614f1c7ea27c3
CrazyLab1001 <cramerkj@mail.uc.edu>
2025-03-01 16:13:11 -0500

    Tasks 1-4 completed! Added new program for Nested Loops (Tasks 5-7)

--- src/CountingLoops.java
+++ src/CountingLoops.java
@@ -1,5 +1,21 @@
 public class CountingLoops {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int count;
+        for (count = 0; count < 31; count++) {
+            System.out.print(count + "  ");
+        }
+        System.out.println("");
+        for (count = 30; count > -1; count--){
+            System.out.print(count + "  ");
+        }
+        System.out.println("");
+        for (count = 0; count < 19; count = count + 3)
+        {
+            System.out.print(count + "  ");
+        }
+        System.out.println("");
+        for (count = 10; count > -1; count = count - 2){
+            System.out.print(count + "  ");
+        }
     }
 }
---
+++ src/NestedForLoops.java
@@ -0,0 +1,5 @@
+public class NestedForLoops {
+    public static void main(String[] args) {
+
+    }
+}
